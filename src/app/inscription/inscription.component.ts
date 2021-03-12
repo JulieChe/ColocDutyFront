03a77bcad0af5f2c9098139587fbbdea3c72bdc1
JulieChe <chevallierjulie8@gmail.com>
2021-03-12 16:02:25 +0100

    mise à jour inscription back et front okey

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,3 +1,4 @@
+import { NgIf } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
@@ -11,21 +12,27 @@ export class InscriptionComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   user;
+  msg = null;
 
   ngOnInit(): void {
   }
 
   userCreate(user): void {
     console.log('afficher le user ', user);
-    this.http.post('http://localhost:8085/saveuser', user).subscribe(() => {
-      // On a créé l'utilisateur, on retourne sur la page de connexion
-      console.log('user créé');
-
-    }, err => {
-      // L'utilisateur n'a pas été créé on affiche un message d'erreur
-      console.log('erreur création user' + err);
-    });
-
+    if (user.password === user.password2) {
+      this.http.post('http://localhost:8085/saveuser', user).subscribe(() => {
+        // On a créé l'utilisateur, on retourne sur la page de connexion
+        console.log('user créé');
+        this.msg = 'Utilisateur créé';
+      }, err => {
+        // L'utilisateur n'a pas été créé on affiche un message d'erreur
+        console.log('erreur création user' + err);
+        this.msg = 'Erreur création du nouvel utilisateur !';
+      });
+    } else {
+      this.msg = 'Les mots de passe sont différents !';
+      console.log('Attention, les mdp sont différents');
+    }
   }
 
 }
