8181ec59a039cd96c388aa7b66abe04bb1deb366
Adelie97 <adelie3221@gmail.com>
2021-03-15 10:17:25 +0100

    résolution du merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './connexion/connexion.component';
-<<<<<<< HEAD
+import { InscriptionComponent } from './inscription/inscription.component';
 import { TestComponent } from './test/test.component';
 
 
 const routes: Routes = [
   {component: ConnexionComponent, path: 'connexion'},
-  {component: TestComponent, path: 'test'}
-  
-=======
-import { InscriptionComponent } from './inscription/inscription.component';
-
-const routes: Routes = [
-  {component: ConnexionComponent, path: 'connexion'},
+  {component: TestComponent, path: 'test'},
   {component: InscriptionComponent, path: 'inscription'}
->>>>>>> 5a09c6f39d13ce0d88b22cf27840fbd6f4237732
+  
 ];
 
 @NgModule({
