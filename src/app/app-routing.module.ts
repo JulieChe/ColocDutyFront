01b74a8acfae31d619e21901c4a4dd62279754ff
Adelie97 <adelie3221@gmail.com>
2021-03-15 14:30:36 +0100

    Début page profil public coloc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { TestComponent } from './test/test.component';
+import { PublicColocComponent } from './public-coloc/public-coloc.component';
 
 
 const routes: Routes = [
   {component: ConnexionComponent, path: 'connexion'},
   {component: TestComponent, path: 'test'},
   {component: InscriptionComponent, path: 'inscription'},
+  {component: PublicColocComponent, path: 'public-coloc'},
   {path: '', redirectTo: 'connexion', pathMatch: 'full'},
 
 ];
