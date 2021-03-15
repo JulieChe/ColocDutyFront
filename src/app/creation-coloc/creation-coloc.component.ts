1ff59d8151d3084b4a47f25cb9c629c4f8b0c66f
thierry308 <thierrysalmon9@gmail.com>
2021-03-15 14:38:07 +0100

    Cretioin coloc fonctionnel

--- src/app/creation-coloc/creation-coloc.component.ts
+++ src/app/creation-coloc/creation-coloc.component.ts
@@ -1,4 +1,7 @@
+
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-creation-coloc',
@@ -7,9 +10,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreationColocComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient,private router: Router ) { }
+
+
+  msg = null;
+
 
   ngOnInit(): void {
   }
 
+
+  // adresseCreate(adresse): void {
+  //   this.http.post('http://localhost:8085/saveAdresse', adresse).subscribe(() => {
+  //     console.log('Adresse créée');
+  //   })
+  // }
+
+  colocCreate(coloc): void {
+    this.http.post('http://localhost:8085/savecoloc', coloc).subscribe(() => {
+      console.log('coloc créée');
+
+    }, err => {
+      // L'utilisateur n'a pas été créé on affiche un message d'erreur
+      console.log('erreur création de coloc' + err);
+    });
+  }
+
+
+  toutCreate(value) {
+    console.log("debut",value);
+   
+    const adresse ={
+      numVoie: value.numVoie,
+      nomRue: value.nomRue,
+      ville: value.ville,
+     codePostal:value.codePostal
+    }
+
+
+    // this.adresse.numVoie = value.numVoie;
+    // this.adresse.nomRue = value.nomRue;
+    // this.adresse.ville = value.ville;
+
+    const coloc = {
+      nomColoc: value.nomColoc,
+      descColoc: value.descColoc,
+      capacite: value.capacite,
+      loyer: value.loyer,
+      public: value.public,
+      adresse: adresse
+    }
+    
+    
+    console.log("fin",coloc);
+
+    // this.adresseCreate(this.adresse);
+    this.colocCreate(coloc);
+
+    this.router.navigateByUrl('/accueil')
+
+
+  }
 }
+
+
+//   userCreate(user): void {
+
+//       this.http.post('http://localhost:8085/saveuser', user).subscribe(() => {
+//         // On a créé l'utilisateur, on retourne sur la page de connexion
+//         console.log('user créé');
+//         this.msg = 'Utilisateur créé';
+//        err => {
+//         // L'utilisateur n'a pas été créé on affiche un message d'erreur
+//         console.log('erreur création user' + err);
+//         this.msg = 'Erreur création du nouvel utilisateur !';
+//       });
+//     } else {
+//       this.msg = 'Les mots de passe sont différents !';
+//       console.log('Attention, les mdp sont différents');
+//     }
+//   }
+
+// }
+
