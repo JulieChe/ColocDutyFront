b5d893b6f5ecab09796d01f19c2353dce18e51e2
thierry308 <thierrysalmon9@gmail.com>
2021-03-17 11:04:12 +0100

    test3

--- src/app/creation-tache/creation-tache.component.ts
+++ src/app/creation-tache/creation-tache.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AutheService } from '../services/authe.service';
 
 @Component({
   selector: 'app-creation-tache',
@@ -8,9 +9,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CreationTacheComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authe: AutheService) { }
 
   tache;
+  idColoc;
+  currentUser;
+  idUser;
 
   ngOnInit(): void {
   }
@@ -19,11 +23,40 @@ export class CreationTacheComponent implements OnInit {
     this.http.post('http://localhost:8085/savetache', tache).subscribe({
       next: (data) => {
         this.tache = data;
+        this.tache.idColoc=this.idColoc;
         console.log(this.tache);
-        console.log(this.tache.ponctuel.checked);
+        
       },
     });
   }
+
+  RecupIdColoc(idUser): void {
+    this.http.post('http://localhost:8085/getidColocbyidUser',idUser).subscribe({
+      next: (data2) => {
+        this.idColoc = data2;
+        console.log(this.idColoc);
+      },
+    });
+  }
+
+  tout(value) {
+
+    const coloc = {
+      nomTache: value.nomtache,
+      nbEtoiles: value.nbEtoiles,
+      date: value.date,
+      frequence: value.frequence,
+     
+    }
+
+    this.tacheCreate(this.tache);
+    
+    this.currentUser = this.authe.getUserCo();
+    this.idUser =9;
+    
+    this.RecupIdColoc(this.idUser);
+    
+    }
 
 }
 
